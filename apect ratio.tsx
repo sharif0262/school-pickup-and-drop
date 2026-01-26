2d77e007c412e93a33f7fe73fe51216c1f65128d
sharif0262 <saquibshaik0262@gmail.com>
2026-01-26 17:25:58 +0530

    Update apect ratio.tsx

--- apect ratio.tsx
+++ apect ratio.tsx
@@ -1,9 +1,26 @@
 'use client';
 
+import * as React from 'react';
 import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
 
-function AspectRatio({ ...props }: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
-    return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />;
+interface AspectRatioProps extends React.ComponentProps<typeof AspectRatioPrimitive.Root> {
+  /** Aspect ratio (width / height). Defaults to 1 (square) */
+  ratio?: number;
 }
 
+const AspectRatio = React.forwardRef<
+  React.ElementRef<typeof AspectRatioPrimitive.Root>,
+  AspectRatioProps
+>(({ ratio = 1, ...props }, ref) => (
+  <AspectRatioPrimitive.Root
+    ref={ref}
+    data-slot="aspect-ratio"
+    ratio={ratio}
+    {...props}
+  />
+));
+
+AspectRatio.displayName = 'AspectRatio';
+
 export { AspectRatio };
+
